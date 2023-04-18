5478d0b49ae6e4f5526ea3f3c53eab4d284ba94f
gilang.sukma <gilang.sukma@privyid.tech>
2023-04-18 10:46:36 +0700

    [Fixed] done and delete button

--- src/components/card/todoCard.js
+++ src/components/card/todoCard.js
@@ -66,66 +66,73 @@ const TodoCard = ({ id, color, title, status }) => {
             )}
           </div>
           <div className="col-span-3 justify-self-end p-2">
-            {statusState === "done" ? (
-              <button
-                onClick={handleUndone}
-                className="rounded-full rounded border border-2 border-cyan-700 bg-cyan-700 text-white hover:text-transparent hover:bg-transparent"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={3}
-                  stroke="currentColor"
-                  className="w-6 h-6 py-1 px-1"
+            <div class="grid grid-rows-2 grid-flow-col gap-1">
+              <div>
+                {statusState === "done" ? (
+                  <button
+                    onClick={handleUndone}
+                    className="rounded-full rounded border border-2 border-cyan-700 bg-cyan-700 text-white hover:text-transparent hover:bg-transparent"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={3}
+                      stroke="currentColor"
+                      className="w-6 h-6 py-1 px-1"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M4.5 12.75l6 6 9-13.5"
+                      />
+                    </svg>
+                  </button>
+                ) : (
+                  <button
+                    onClick={handleDone}
+                    className="rounded-full rounded border border-2 border-cyan-700 text-transparent hover:text-white hover:bg-cyan-700"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={3}
+                      stroke="currentColor"
+                      className="w-6 h-6 py-1 px-1"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M4.5 12.75l6 6 9-13.5"
+                      />
+                    </svg>
+                  </button>
+                )}
+              </div>
+
+              <div>
+                <button
+                  onClick={handleDelete}
+                  className="rounded-full bg-transparent border border-2 border-red-900 hover:bg-red-700 text-red-900 font-semibold hover:text-white"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4.5 12.75l6 6 9-13.5"
-                  />
-                </svg>
-              </button>
-            ) : (
-              <button
-                onClick={handleDone}
-                className="rounded-full rounded border border-2 border-cyan-700 text-transparent hover:text-white hover:bg-cyan-700"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={3}
-                  stroke="currentColor"
-                  className="w-6 h-6 py-1 px-1"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4.5 12.75l6 6 9-13.5"
-                  />
-                </svg>
-              </button>
-            )}
-            <button
-              onClick={handleDelete}
-              className="rounded-full bg-transparent border border-2 border-red-900 hover:bg-red-700 text-red-900 font-semibold hover:text-white"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={3}
-                stroke="currentColor"
-                className="w-6 h-6 py-1 px-1"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={3}
+                    stroke="currentColor"
+                    className="w-6 h-6 py-1 px-1"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
